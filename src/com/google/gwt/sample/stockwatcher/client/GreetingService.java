9cb1071c54011c1d86beecd90e5ff4b5e605f828
keener <kol.bobbyjoe@gmail.com>
2014-09-21 16:35:08 -0700

    changed name of interface

--- src/com/google/gwt/sample/stockwatcher/client/GreetingService.java
+++ src/com/google/gwt/sample/stockwatcher/client/GreetingService.java
@@ -7,7 +7,7 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
  * The client-side stub for the RPC service.
  */
 @RemoteServiceRelativePath("greet")
-public interface GreetingService extends RemoteService {
+public interface GreetingServiceMaker extends RemoteService {
 	String greetServer(String name) throws IllegalArgumentException;
 	String goodbyeServer(String name) throws IllegalArgumentException;
 }
